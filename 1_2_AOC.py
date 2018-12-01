ef4acaac9aeec7ed0bd29b758cade4ec82c7d699
Vikenes <45514390+Vikenes@users.noreply.github.com>
2018-12-01 18:25:30 +0100

    Update 1_2_AOC.py

--- 1_2_AOC.py
+++ 1_2_AOC.py
@@ -5,7 +5,7 @@ def readfile2(filename):
     noi = 0 # number of iterations
     infile = open(filename, 'r')
     iteration = []
-    while True:
+    while total not in iteration:
         for line in infile:
             noi += 1
             print(noi)
@@ -13,16 +13,13 @@ def readfile2(filename):
             if line.find('+') != -1:
                 total += int(line[1:-1])
                 if total in iteration:
-                    print('The sequence first reaches %g twice' %total)
-                    sys.exit()
-
+                    sys.exit(print('The sequence first reaches %g twice' %total))
                 iteration.append(total)
 
             elif line.find('-') != -1:
                 total -= int(line[1:-1])
                 if total in iteration:
-                    print('The sequence first reaches %g twice' %total)
-                    sys.exit()
+                    sys.exit(print('The sequence first reaches %g twice' %total))
                 iteration.append(total)
 
 readfile2('data_1_1.txt')
