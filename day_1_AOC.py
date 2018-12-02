48e3e39fd2a76aa0ae2a86486edeee1cfb5e29ba
Vikenes <45514390+Vikenes@users.noreply.github.com>
2018-12-02 23:17:33 +0100

    Update and rename 1_2_AOC.py to day_1_AOC.py

--- day_1_AOC.py
+++ day_1_AOC.py
@@ -2,14 +2,12 @@ import sys
 
 def readfile2(filename):
     total = 0
-    noi = 0 # number of iterations
     infile = open(filename, 'r')
     iteration = []
     while True:
         infile.seek(0,0)
         for line in infile:
             if line.find('+') != -1:
-
                 total += int(line[1:-1])
                 if int(total) in iteration:
                     sys.exit(print('The sequence first reaches %g twice' %total))
