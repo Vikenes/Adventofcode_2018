d5844f5180530cf751814a9d237a81a3095bf4d2
Vikenes <45514390+Vikenes@users.noreply.github.com>
2018-12-03 13:13:47 +0100

    Update day_2_AOC.py

--- day_2_AOC.py
+++ day_2_AOC.py
@@ -15,3 +15,24 @@ for value in data:
         break
 
 print(two*three)
+
+# part 2 
+import sys
+
+f = open('data_2.txt', 'r')
+data = [(line[:-1]) for line in f]
+
+for i in range(len(data)):
+    for k in range(len(data)):
+        nod = 0 # number of differences
+        for j in range(len(data[0])):
+            if data[i][j] != data[k][j]:
+                nod += 1
+
+        if nod == 1:
+            a = data[k]
+            c = data[i]
+            for m in range(len(data[i])):
+                if a[m] != c[m]:
+                    d = c[:m] + c[m+1:]
+            sys.exit(d)
